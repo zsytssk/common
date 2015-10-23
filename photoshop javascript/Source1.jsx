0dc02cf32cd4b5bf2f0be25e0d9751854adfd700
张世阳 <zsytssk@gmail.com>
2015-10-23 18:24:25 +0800

    继续修改
    继续修改

--- photoshop javascript/Source1.jsx
+++ photoshop javascript/Source1.jsx
@@ -1,46 +1,76 @@
-﻿function PathCoordinateExport(el, fileOutput, filePath) {
+﻿function getTextPositions(el, fileOut, path) {
+	// Get the layers
+	var layers = el.layers;
 
-	var layerRef = app.activeDocument.artLayers.getByName("图层 2");
-	alert(app.activeDocument.artLayers.length)
-	layerRef.allLocked = true;
+	// Loop 'm
+	for(var layerIndex = layers.length; layerIndex > 0; layerIndex--) {
+
+		// curentLayer ref
+		var currentLayer = layers[layerIndex - 1];
+
+		// currentLayer is a LayerSet
+		if(currentLayer.typename == "LayerSet") {
+
+			goTextExport2(currentLayer, fileOut, path + currentLayer.name + '/');
+
+			// currentLayer is not a LayerSet
+		} else {
+
+			// Layer is visible and Text --> we can haz copy paste!
+			if((currentLayer.visible) && (currentLayer.kind == LayerKind.TEXT)) {
+				fileOut.writeln('***********************');
+				fileOut.writeln('');
+				fileOut.writeln('LayerPath: ' + path);
+				fileOut.writeln('LayerName: ' + currentLayer.name);
+				fileOut.writeln('');
+				log(currentLayer.bounds);
+				fileOut.writeln('left:' + currentLayer.bounds[0]);
+				fileOut.writeln('top:' + currentLayer.bounds[1]);
+				fileOut.writeln('');
+			}
+		}
+
+
+	}
+
+
 
 }
 
 function init() {
 
 	// Establish the correct linefeed type
-	if ($.os.search(/windows/i) != -1) {
+	if($.os.search(/windows/i) != -1) {
 		fileLineFeed = "windows";
 	} else {
 		fileLineFeed = "macintosh";
 	}
 
 	// Do we have a document open?
-	if (app.documents.length === 0) {
+	if(app.documents.length === 0) {
 		alert("Please open a file", "PathsToImageMap Error", true);
 		return;
 	}
 
 	// If we have more than one document open...
-	if (app.documents.length > 1) {
+	if(app.documents.length > 1) {
 
 		var runMultiple = confirm("PathsToImageMap has detected multiple open files.\nDo you wish to run PathsToImageMap on all open files?", true, "PathsToImageMap");
 
-		if (runMultiple === true) {
+		if(runMultiple === true) {
 			docs = app.documents;
 		} else {
 			docs = [app.activeDocument];
 		}
 
-	// Or only one document open...
+		// Or only one document open...
 	} else {
 		runMultiple = false;
 		docs = [app.activeDocument];
 	}
 
 	// Loop through all documents
-	for (var i = 0; i < docs.length; i++)
-	{
+	for(var i = 0; i < docs.length; i++) {
 
 		// Auto set filePath and fileName
 		filePath = Folder.myDocuments + '/PathsToImageMap-' + docs[i].name + '.txt';
@@ -58,7 +88,7 @@ function init() {
 		app.activeDocument = docs[i];
 
 		// call to the core with the current document
-		PathCoordinateExport(app.activeDocument, fileOutput, '/');
+		getTextPositions(app.activeDocument, fileOutput, '/');
 
 		// close the file
 		fileOutput.close();
@@ -66,7 +96,7 @@ function init() {
 	}
 
 	// Finish up by displaying a notice (multiple) or opening the output file (single)
-	if (runMultiple === true) {
+	if(runMultiple === true) {
 		alert("PathsToImageMap has parsed " + documents.length + " files;\nFiles were saved in your documents folder", "PathsToImageMap");
 	} else {
 		fileOutput.execute();
