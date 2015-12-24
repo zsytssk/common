e92b801777b4327b22dc4f98f1e5dedfb9524a93
张世阳 <zsytssk@gmail.com>
2015-12-24 10:27:15 +0800

    add select time

---
+++ www/html/select time/js/selecttime.js
@@ -0,0 +1,69 @@
+// 用top代替scrollTop
+// 最后可以用 css animate 实现平滑滚动
+
+NodeList.prototype.forEach = Array.prototype.forEach;
+
+var _stItems = document.querySelectorAll('.st-item');
+var _stItem_lis = document.querySelector('.pop-selecttime .st-list .st-item li');
+_stItems.forEach(function (element) {
+	var touchY = 0;
+	var deltaY = 0;
+	var itemH = _stItem_lis.offsetHeight;
+	var _inner;
+
+	element.addEventListener('touchstart', function (event) {
+		var touch = event.touches[0] || event.changedTouches[0];
+
+		this.classList.add('touchstart');
+		_inner = this.querySelector('.inner');
+		touchY = touch.pageY;
+	});
+
+	element.addEventListener('touchmove', function (event) {
+		if(!this.classList.contains('touchstart') && !this.classList.contains('touchmove')) {
+			return true;
+		}
+		var touch = event.touches[0] || event.changedTouches[0];
+		event.preventDefault();
+		if(this.classList.contains('touchstart')) {
+			this.classList.add('touchmove');
+			this.classList.remove('touchstart');
+		}
+		var clientY = touch.clientY;
+		deltaY = clientY - touchY;
+		touchY = clientY;
+		_inner.scrollTop -= deltaY;
+
+	});
+
+	element.addEventListener('touchend', function (event) {
+		this.classList.remove('touchmove');
+		inittouch();
+	});
+
+	function inittouch() {
+		var scrollTop = itemH * (Math.round(_inner.scrollTop / itemH));
+
+		scrollTo(_inner, scrollTop, 300);
+		_inner = '';
+		element.classList.remove('touchmove');
+		touchY = 0;
+		deltaY = 0;
+	}
+
+	function scrollTo(element, to, duration) {
+		if(duration <= 0) {
+			return;
+		}
+		var difference = to - element.scrollTop;
+		var perTick = difference / duration * 10;
+
+		setTimeout(function () {
+			element.scrollTop = element.scrollTop + perTick;
+			if(element.scrollTop == to) {
+				return;
+			}
+			scrollTo(element, to, duration - 10);
+		}, 10);
+	}
+});
--- www/html/uploadimg/js/uploadimg.js
+++ www/html/uploadimg/js/uploadimg.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 	$('.uploadimg-box input[type=file]').on('change', function (event) {
 		var $box = $(this).parent();
 		var $input = $(this);
-		var $img = $(this).siblings('img');
+		var $img = $(this).siblings('.uploadimg');
 		var oPreviewImg = null,
 			oFReader = new window.FileReader(),
 			rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
@@ -50,6 +50,6 @@ $(document).ready(function () {
 
 	function initUploadBox(obj) {
 		$(obj).removeClass('uploading, uploaded');
-		$(obj).find('img').attr('src', '');
+		$(obj).find('.uploadimg').attr('src', '');
 	}
 });
