5117a8708caae0ef4ec99e64fed61e2c77f32619
zsytssk <zsytssk@gmail.com>
2016-06-08 09:41:32 +0800

    test

--- www/html/uploadimg/modernbrowser/js/uploadimg.js
+++ www/html/uploadimg/modernbrowser/js/uploadimg.js
@@ -1,52 +1,52 @@
 $(document).ready(function () {
-	$('.uploadimg-box .delete').on('click', function (event) {
-		initUploadBox($(this).parent());
-	});
-	$('.uploadimg-box input[type=file]').on('change', function (event) {
-		var $box = $(this).parents('li');
-		var $input = $(this);
-		var $img = $box.find('.uploadimg');
-		var oPreviewImg = null,
-			oFReader = new window.FileReader(),
-			rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
+  $('.uploadimg-box .delete').on('click', function (event) {
+    initUploadBox($(this).parent());
+  });
+  $('.uploadimg-box input[type=file]').on('change', function (event) {
+    var $box = $(this).parents('li');
+    var $input = $(this);
+    var $img = $box.find('.uploadimg');
+    var oPreviewImg = null,
+      oFReader = new window.FileReader(),
+      rFilter = /^(?:image\/bmp|image\/cis\-cod|image\/gif|image\/ief|image\/jpeg|image\/jpeg|image\/jpeg|image\/pipeg|image\/png|image\/svg\+xml|image\/tiff|image\/x\-cmu\-raster|image\/x\-cmx|image\/x\-icon|image\/x\-portable\-anymap|image\/x\-portable\-bitmap|image\/x\-portable\-graymap|image\/x\-portable\-pixmap|image\/x\-rgb|image\/x\-xbitmap|image\/x\-xpixmap|image\/x\-xwindowdump)$/i;
 
-		oFReader.onloadstart = function (e) {
-			$box.addClass('uploading');
-		};
-		oFReader.onerror = function (evt) {
-			initUploadBox($box);
-			switch(evt.target.error.code) {
-			case evt.target.error.NOT_FOUND_ERR:
-				alert('File Not Found!');
-				break;
-			case evt.target.error.NOT_READABLE_ERR:
-				alert('File is not readable');
-				break;
-			case evt.target.error.ABORT_ERR:
-				break; // noop
-			default:
-				alert('An error occurred reading this file.');
-			}
-		};
-		oFReader.onload = function (oFREvent) {
-			console.log(oFREvent.target.result);
-			$img.attr('src', oFREvent.target.result);
-			$box.addClass('uploaded').removeClass('uploading');
-		};
+    oFReader.onloadstart = function (e) {
+      $box.addClass('uploading');
+    };
+    oFReader.onerror = function (evt) {
+      initUploadBox($box);
+      switch (evt.target.error.code) {
+      case evt.target.error.NOT_FOUND_ERR:
+        alert('File Not Found!');
+        break;
+      case evt.target.error.NOT_READABLE_ERR:
+        alert('File is not readable');
+        break;
+      case evt.target.error.ABORT_ERR:
+        break; // noop
+      default:
+        alert('An error occurred reading this file.');
+      }
+    };
+    oFReader.onload = function (oFREvent) {
+      $img.attr('src', oFREvent.target.result);
+      $box.addClass('uploaded').removeClass('uploading');
+    };
 
-		var aFiles = $input[0].files;
-		if(aFiles.length === 0) {
-			return;
-		}
-		if(!rFilter.test(aFiles[0].type)) {
-			alert('You must select a valid image file!');
-			return;
-		}
-		oFReader.readAsDataURL(aFiles[0]);
-	});
+    var aFiles = $input[0].files;
+    if (aFiles.length === 0) {
+      return;
+    }
+    console.log(aFiles[0].size > 4 * 1024 * 1024);
+    if (!rFilter.test(aFiles[0].type)) {
+      alert('You must select a valid image file!');
+      return;
+    }
+    oFReader.readAsDataURL(aFiles[0]);
+  });
 
-	function initUploadBox(obj) {
-		$(obj).removeClass('uploading, uploaded');
-		$(obj).find('.uploadimg').attr('src', '');
-	}
+  function initUploadBox(obj) {
+    $(obj).removeClass('uploading, uploaded');
+    $(obj).find('.uploadimg').attr('src', '');
+  }
 });
