755809e2061c327c66510f7046ff5ef6fe7c157f
张世阳 <zsytssk@gmail.com>
2017-11-30 15:02:56 +0800

    master:path:: fix some bug

---
+++ photoshop javascript/shoal/analysisShoal1PSD.js
@@ -0,0 +1,87 @@
+﻿/*
+  {
+    typeId: number,
+    startPos: {x: number, y: number}
+  }
+*/
+
+#target photoshop;
+#include "../libs/json2.js";
+
+var origUnits = app.preferences.rulerUnits;
+app.preferences.rulerUnits = Units.PIXELS;
+
+var activeDoc = app.activeDocument;
+if (typeof activeDoc.path == 'undefined') {
+  var myPath = 'c:';
+} else {
+  var myPath = app.activeDocument.path;
+}
+filePath = (new File($.fileName)).parent + '/shoal1.data.json';
+var f = new File(filePath);
+f.encoding = 'UTF8';
+f.open('w');
+
+var shoal = {
+  bounds: {
+    width: parseInt(activeDoc.width),
+    height: parseInt(activeDoc.height)
+  }
+};
+var fish = [];
+
+function getAlllayersInfo(layerSet, fishType) {
+  var fish = [];
+  var layers = layerSet.artLayers;
+
+  for (var l_len = layers.length, j = l_len-1; j>=0; j--) {
+    var layer = layers[j];
+    var name  = parseInt(layer.name);
+    var isSpecial = (layer.name == 'special');
+    var fishType = name ? name : (fishType ? fishType : '');
+    if (!fishType) {
+      continue;
+    }
+
+    var fishInfo = getLayerInfo(layer, fishType, isSpecial);
+    fish.push(fishInfo);
+  }
+
+  var layerSets = layerSet.layerSets;
+  for (var l_len = layerSets.length, j = l_len-1; j>=0; j--) {
+    var layerSet = layerSets[j];
+    var name  = parseInt(layerSet.name);
+    var fishType = name ? name : fishType;
+    var layerSet_fish = getAlllayersInfo(layerSet, fishType);
+    fish = fish.concat(layerSet_fish);
+  }
+
+  return fish;
+}
+
+shoal.fish = getAlllayersInfo(activeDoc);
+f.writeln(JSON.stringify(shoal));
+
+f.close('w');
+app.preferences.rulerUnits = origUnits;
+
+function getLayerInfo(layer, fishType, isSpecial) {
+  var bounds = layer.bounds;
+  var x1 = parseInt(bounds[0]);
+  var y1 = parseInt(bounds[1]);
+  var x2 = parseInt(bounds[2]);
+  var y2 = parseInt(bounds[3]);
+
+  var x = x1 + (x2 - x1)/2;
+  var y = y1 + (y2 - y1)/2;
+
+  $.writeln(fishType);
+  return {
+    isSpecial: isSpecial,
+    typeId: fishType,
+    startPos: {
+      x: x,
+      y: y
+    }
+  };
+}
